7805c236b2ec5e89b999430521f139b96ae27ec9
Nikita885 <faikqweas@mail.ru>
2024-09-25 01:38:42 +0500

    избавился от не нужного кода

--- Bd_Creator/Bd_Creator/urls.py
+++ Bd_Creator/Bd_Creator/urls.py
@@ -1,18 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from api.views import register, CustomLoginView, logout_view, home_view
-from django.conf.urls import handler404
-from django.shortcuts import redirect
-from django.conf import settings
-from django.conf.urls.static import static
-from django.core.exceptions import PermissionDenied
 
 
-def handler404(request, exception):
-    if settings.DEBUG:
-        # Если в DEBUG, используем стандартный обработчик
-        raise PermissionDenied
-    return redirect('home')
+
 urlpatterns = [
     path('', home_view, name='home'),
     path('admin/', admin.site.urls),
@@ -20,11 +11,5 @@ urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', logout_view, name='logout'),
     path('api/', include('api.urls')),
-
 ]
 
-if not settings.DEBUG:
-    print(settings.DEBUG, 123)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-handler404 = handler404
--- Bd_Creator/api/middleware.py
+++ Bd_Creator/api/middleware.py
@@ -1,8 +1,6 @@
 from django.http import HttpResponseRedirect
 import logging
 
-logger = logging.getLogger(__name__)
-
 def redirect_on_404(get_response):
     def middleware(request):
         response = get_response(request)
@@ -13,23 +11,15 @@ def redirect_on_404(get_response):
 def login_required_middleware(get_response):
     def middleware(request):
         token = request.COOKIES.get('access_token')
-        
-        # Пропускаем запросы к статическим файлам
-        if request.path.startswith('/static/') or request.path.startswith('/admin/'):
-            #print(request.path)
-            logger.debug("Пропускаем запрос к статическому файлу.")
-            return get_response(request)
 
         # Если токен есть, значит пользователь авторизован
         if token:
             if request.path in ['/login/', '/register/']:
-                logger.debug("Пользователь авторизован, перенаправление на главную.")
                 return HttpResponseRedirect('/')  # Перенаправляем на главную страницу
         else:
             if request.path in ['/login/', '/register/']:
                 return get_response(request)
             else:
-                logger.debug("Токена нет, перенаправление на страницу логина.")
                 return HttpResponseRedirect('/login/')  # Перенаправляем на страницу авторизации
         
         return get_response(request)
